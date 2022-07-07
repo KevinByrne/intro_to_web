8f5c5446ad3161eb2165e7c389c5c3888bdf054d
KevinByrne <kevbee79@gmail.com>
2022-07-07 12:18:55 +0100

    additional params added to app.rb and index.erb file updated

--- app.rb
+++ app.rb
@@ -17,7 +17,15 @@ get '/another-one' do
   'we have created another route'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond", "Tibbles", "Scratchula"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @colour = params[:colour]
+  @personality = params[:personality]
   erb(:index)
 end
